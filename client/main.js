775b306402c0db599a0b7c9b264cc5058ca48aab
SMARTIUM <dev@smartium.io>
2016-12-12 04:49:55 -0200

    include SMARTIUM logo

--- client/main.js
+++ client/main.js
@@ -3,6 +3,9 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+Meteor.startup(() => {
+})
+
 Template.body.onCreated(function bodyOnCreated() {
   varTemplate = new ReactiveVar({template: 'home'});
 });
